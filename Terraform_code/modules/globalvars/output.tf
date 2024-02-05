9814ce4bde755ed40141905cbcd66c84381bc339
Prason Giri <153341265+145957221-myseneca@users.noreply.github.com>
2024-02-05 18:15:04 -0500

    adds terraform codes

---
+++ Terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "pgiri5"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "Assignment1"
+}
