0c19168a977ca6dc112c0b6def6eebe21750c90d
Prason Giri <153341265+145957221-myseneca@users.noreply.github.com>
2024-02-06 00:11:10 -0500

    Update install.sh

--- Terraform_code/dev/instances/install.sh
+++ Terraform_code/dev/instances/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
     sudo yum update -y
     sudo yum install docker -y
-    sudo yum install git -y
     sudo yum install mysql -y
     sudo systemctl start docker
     sudo usermod -a -G docker ec2-user
